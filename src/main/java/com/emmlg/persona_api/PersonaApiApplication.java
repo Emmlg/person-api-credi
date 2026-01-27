d53be468ca4439e5f792a8ec53b9eed9ba8c7e81
Emmanuel Lopez <emmanuelg.dev@gmail.com>
2026-01-27 15:50:26 -0600

    core : implement logs and swagger configuration

--- src/main/java/com/emmlg/persona_api/PersonaApiApplication.java
+++ src/main/java/com/emmlg/persona_api/PersonaApiApplication.java
@@ -1,7 +1,11 @@
 package com.emmlg.persona_api;
 
+import org.modelmapper.Conditions;
+import org.modelmapper.ModelMapper;
+import org.modelmapper.convention.MatchingStrategies;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class PersonaApiApplication {
@@ -10,4 +14,11 @@ public class PersonaApiApplication {
 		SpringApplication.run(PersonaApiApplication.class, args);
 	}
 
+	@Bean
+    public ModelMapper loadMapper() {
+        var modelMapper = new ModelMapper();
+        modelMapper.getConfiguration().setMatchingStrategy(MatchingStrategies.STRICT);
+        modelMapper.getConfiguration().setPropertyCondition(Conditions.isNotNull());
+        return modelMapper;
+    }
 }
