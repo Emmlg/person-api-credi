5605eb8deaab7aecd9dfad1a898ff1cba1cd706d
Emmanuel Lopez <emmanuelg.dev@gmail.com>
2026-01-29 10:44:29 -0600

    fix: improve exception handler fields

--- src/main/java/com/emmlg/persona_api/exceptions/PersonException.java
+++ src/main/java/com/emmlg/persona_api/exceptions/PersonException.java
@@ -3,10 +3,19 @@ package com.emmlg.persona_api.exceptions;
 import lombok.Getter;
 import org.springframework.http.HttpStatus;
 
+import java.util.List;
+
 @Getter
 public class PersonException extends RuntimeException {
 
-    private String process;
     private HttpStatus httpStatus;
+    private String process;
+
+
+    public PersonException(String message, String process, HttpStatus httpStatus) {
+        super(message);
+        this.process = process;
+        this.httpStatus = httpStatus;
+    }
 
 }
--- src/main/java/com/emmlg/persona_api/exceptions/PersonExceptionController.java
+++ src/main/java/com/emmlg/persona_api/exceptions/PersonExceptionController.java
@@ -3,12 +3,17 @@ package com.emmlg.persona_api.exceptions;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.method.annotation.MethodArgumentConversionNotSupportedException;
 import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
+
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 @Slf4j
 @RestControllerAdvice
@@ -20,20 +25,38 @@ public class PersonExceptionController {
 
         return ResponseEntity.status(ex.getHttpStatus())
                 .body(CommonErrorResponse.builder()
+                        .httpStatus(ex.getHttpStatus())
                         .message(ex.getMessage())
                         .process(ex.getProcess())
-                        .httpStatus(ex.getHttpStatus())
                         .build());
     }
 
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public CommonErrorResponse handleValidationExceptions(MethodArgumentNotValidException ex) {
+
+        List<String> errors = ex.getBindingResult()
+                .getFieldErrors()
+                .stream()
+                .map(err -> err.getField() + ": " + err.getDefaultMessage())
+                .toList();
+
+        return CommonErrorResponse.builder()
+                .message("Validation failed")
+                .process("REQUEST_VALIDATION")
+                .httpStatus(HttpStatus.BAD_REQUEST)
+                .errors(errors)
+                .build();
+    }
+
     @ExceptionHandler({
-            MethodArgumentNotValidException.class,
             MethodArgumentTypeMismatchException.class,
             MethodArgumentConversionNotSupportedException.class
     })
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     public CommonErrorResponse handleAllArgumentExceptions(Exception exception) {
         log.error(exception.getMessage());
+
         return CommonErrorResponse.builder()
                 .message("Invalid request parameters")
                 .process("REQUEST_VALIDATION")
